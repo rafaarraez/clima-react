8c477bacde1e7941117b2e0e63ca5ef8bb4e84bf
rafael <rafa.arraez.gue@gmail.com>
2021-01-04 16:17:12 -0400

    Updates

---
+++ precache-manifest.485668395f72e53d2d97d526bb7c6650.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bd426e4feaaeec67a5827efe0cfc0cf",
+    "url": "/clima-react/index.html"
+  },
+  {
+    "revision": "77f69f23bc1401fdef91",
+    "url": "/clima-react/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "e88b27d54cd0dc1e3bc3",
+    "url": "/clima-react/static/js/2.ae3097e4.chunk.js"
+  },
+  {
+    "revision": "f0ad72fb0c1c1e4a268423d8869404c9",
+    "url": "/clima-react/static/js/2.ae3097e4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "77f69f23bc1401fdef91",
+    "url": "/clima-react/static/js/main.b9338b8e.chunk.js"
+  },
+  {
+    "revision": "50913779352b6f88bd5a",
+    "url": "/clima-react/static/js/runtime-main.dad06257.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clima-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
